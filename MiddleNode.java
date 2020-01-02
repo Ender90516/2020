35f31432c096c3bb38bb1aa6937255030b50426d
Ender90516 <55219090+Ender90516@users.noreply.github.com>
2020-01-02 00:52:58 -0500

    Create MiddleNode.java

---
+++ MiddleNode.java
@@ -0,0 +1,15 @@
+/**
+ * class ListNode {
+ *   public int value;
+ *   public ListNode next;
+ *   public ListNode(int value) {
+ *     this.value = value;
+ *     next = null;
+ *   }
+ * }
+ */
+public class Solution {
+  public ListNode middleNode(ListNode head) {
+    // Write your solution here
+  }
+}
